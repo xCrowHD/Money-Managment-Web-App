673a4c24dd25def445c937cd4f672469173c80c5
xCrowHD <102517665+xCrowHD@users.noreply.github.com>
2022-07-11 01:05:37 +0200

    Adding new fetures

--- chartscripts.js
+++ chartscripts.js
@@ -38,7 +38,7 @@
     const myChart = new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: ['👕 Clotes', '🍔 Food', '🚗 Car', '🎉 Fun', '🏠 House'],
+            labels: ['👕 Clothes', '🍔 Food', '🚗 Car', '🎉 Fun', '🏠 House'],
             datasets: [{
                 label: '# of Votes',
                 data: [12, 19, 3, 5, 2, ],
@@ -64,7 +64,7 @@
         options:{
             plugins:{
                 legend:{display:true, position:'right', font:{size:50}},
-                title:{display:true, text:'Where you spent money', font:{size:30}}
+                title:{display:true, text:'Where you spent money', font:{size:15}}
             }
         },
         
@@ -109,7 +109,46 @@
         
 
     });
+    // Balance chart with money spent and earn
+    const ctx3 = document.getElementById('MSE').getContext('2d');
+    const MSE = new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [{
+                label: 'dsfsd',
+                data: [12, -19, -3, 5, -8, 8],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                    
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    
+                ],
+                borderWidth: 1,
+                borderSkipped: false,
+                borderRadius: 20,
 
+            }]
+        },
+        options:{
+            plugins:{
+                legend:{display:false},
+                title:{display:true, text:'How much net money you get earn monthly', font:{size:15}}
+            }
+        },
+        
+
+    });
     
     // function to change months in charts
     function monthS(id, chartnameJS, monthList){
@@ -126,3 +165,5 @@
     monthS('7to12q', MG, ['July', 'August', 'September', 'October', 'November', 'December']);
     monthS('1to6', MS, ['January', 'February', 'March', 'April', 'May', 'June']);
     monthS('7to12', MS, ['July', 'August', 'September', 'October', 'November', 'December']);
+    monthS('1to6w', MSE, ['January', 'February', 'March', 'April', 'May', 'June']);
+    monthS('7to12w', MSE, ['July', 'August', 'September', 'October', 'November', 'December']);

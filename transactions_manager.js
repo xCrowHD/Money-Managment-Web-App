38b4a40acfb6137699fddcc2e46de9c231e65bd0
xCrowHD <102517665+xCrowHD@users.noreply.github.com>
2022-07-12 21:17:41 +0200

    Now the transaction are added in runtime with javascript
    Maybe later ill try to better the code for now thats it

---
+++ transactions_manager.js
@@ -0,0 +1,105 @@
+let number = 0;
+// adding transictions dinamically
+function addingtrans(name, month, day, money, earn_spent, type, descr){
+    // append accordion-item
+    const newitem = document.createElement('div');
+    newitem.className = 'accordion-item';
+    newitem.id = `acc-item${number}`;
+    document.getElementById('accordion').prepend(newitem);
+
+    // header creation
+    const header = document.createElement('h2');
+    header.className = 'accordion-header';
+    header.id = `heading${number}`;
+
+    document.getElementById(`acc-item${number}`).appendChild(header);
+
+    // creating button in header
+    const buttone = document.createElement('button');
+    buttone.className = 'accordion-button accbutton';
+    buttone.id = `accbut${number}`;
+    buttone.type = 'button';
+    buttone.setAttribute('data-bs-toggle', 'collapse');
+    buttone.setAttribute('data-bs-target', `#collapse${number}`);
+
+    document.getElementById(`heading${number}`).appendChild(buttone);
+
+    // adding the row to center evenly
+
+    const row = document.createElement('div');
+    row.className = 'row justify-content-evenly';
+    row.id = `r${number}`;
+    document.getElementById(`accbut${number}`).appendChild(row);
+
+    // adding all the name, month, spent/earn ecc.
+    // creating the block inside the header
+    let i = 0;
+    while (i < 5) {
+        const info = document.createElement('div');
+        info.className = 'col-sm';
+        info.id = `${number}part0${i}`
+        document.getElementById(`r${number}`).appendChild(info)
+    
+        const info1 = document.createElement('div');
+        info1.className = 'input-group mb-3';
+        info1.id = `${number}part1${i}`;
+        document.getElementById(`${number}part0${i}`).appendChild(info1)
+
+        const sp = document.createElement('span');
+        sp.className = "input-group-text";
+        sp.id = `${number}cont${i}`;
+        document.getElementById(`${number}part1${i}`).append(sp);
+
+
+        i++;
+        
+    }
+
+    // creating the person block
+    document.getElementById(`${number}cont0`).textContent = name;
+    // creating the month day block
+    document.getElementById(`${number}cont1`).textContent = month;
+    const dayy = document.createElement('span');
+    dayy.className = 'input-group-text';
+    dayy.textContent = day;
+    document.getElementById(`${number}part11`).appendChild(dayy);
+    // creating the money block
+    document.getElementById(`${number}cont2`).textContent = money;
+        // color change based on spent or earn
+        if(earn_spent == 'Spent'){
+            document.getElementById(`${number}cont2`).style.color = 'red';
+        }
+        else{
+            document.getElementById(`${number}cont2`).style.color = 'green';
+        }
+    const dollarSp = document.createElement('span');
+    dollarSp.className = 'input-group-text';
+    dollarSp.textContent  = '$'
+    document.getElementById(`${number}part12`).appendChild(dollarSp);
+    // creating the earn/spent block
+    document.getElementById(`${number}cont3`).textContent = earn_spent;
+    // creating the person type
+    document.getElementById(`${number}cont4`).textContent = type;
+
+
+    // adding the body of accordion item
+    const body = document.createElement('div');
+    body.className = 'accordion-collapse collapse';
+    body.id = `collapse${number}`;
+    body.setAttribute('aria-labelledby', `heading${number}`);
+    body.setAttribute('data-bs-parent', `#accordion`);
+    document.getElementById(`acc-item${number}`).appendChild(body);
+
+    // creating the text body
+    const tbody = document.createElement('div');
+    tbody.className = 'accordion-body';
+    tbody.textContent = descr;
+    document.getElementById(`collapse${number}`).appendChild(tbody);
+
+      
+    number++;
+
+}
+
+document.getElementById('submitB').addEventListener('click',
+ ()=>{addingtrans('Mattia', 'January', '15', 30, 'Earn', '🎉', 'agfbhgbfkdjhgbfdgakhg')});
